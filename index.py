2d57458272ff95ef40679ad7137a8bb7d3d57fff
ignnoree <153513629+ignnoree@users.noreply.github.com>
2024-11-13 07:11:12 +0300

    Update index.py, prevents dublicate unless bypass

--- index.py
+++ index.py
@@ -6,15 +6,24 @@ from telethon import TelegramClient
 
 api_id='your api'
 api_hash='your api hash'
-
+massage_='massage.txt'
 client=TelegramClient('sessions',api_id,api_hash,)
+
+with open(massage_,'a')as file:
+    last_massage_=set(line.strip()for line in file)
+    
 
 async def getmassage():
     await client.start()
     massage = await client.get_messages('channel_example',limit=1)
     
     message= massage[0].text
-    return message
+    if massage not in last_massage_:
+        return message
+    elif massage in last_massage_ :
+        feedback=input('massage already sent, if you want to bypass that type BYPASS')
+        if feedback == 'BYPASS':
+            return massage
 
 message= asyncio.run(getmassage())
 
@@ -24,6 +33,8 @@ ezgmail.init()
 
 ezgmail.send(f'EXAMPLE@gmail.com','new massages',message)
 
+with open(massage_,'a') as file:
+    file.write(message)
 
 print('Sending the massage...')
 time.sleep(3)
